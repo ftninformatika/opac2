d065febbc0d5927b9713e86f5d077987c7210f56
badf00d21 <jiricekova31@gmail.com>
2019-06-13 11:03:47 +0200

    Home made carousel - finished

--- src/modules/home/components/collection-carousel/collection-carousel.component.ts
+++ src/modules/home/components/collection-carousel/collection-carousel.component.ts
@@ -2,8 +2,23 @@ import { Component, HostListener, Input, OnInit, ViewEncapsulation } from '@angu
 import { Book } from '../../../core/models/book';
 
 export enum DEVICE_WIDTH_BREAKPOINTS {
-  // _8_BOOKS = 1400,
-  _8_BOOKS = 1150
+  _8_BOOKS = 1250,
+  _7_BOOKS = 1050,
+  _6_BOOKS = 850,
+  _5_BOOKS = 780,
+  _4_BOOKS = 650,
+  _3_BOOKS = 520,
+  _2_BOOKS = 370
+}
+
+export enum DEVICE_PREVIEW {
+  WEB = 'web',
+  MOBILE = 'mobile'
+}
+
+export enum ANIMATE_DIRECTION {
+  LEFT = 'left',
+  RIGHT = 'right'
 }
 
 @Component({
@@ -16,17 +31,40 @@ export class CollectionCarouselComponent implements OnInit {
   @Input() books: Book[];
   @Input() title: string;
   public slides: any = [[]];
-  public chunkSize = 4;
+  public chunkSize = 7;
+  public devicePreviewMode = DEVICE_PREVIEW.WEB;
+  public activeSlideIndex = 0;
+  public lastSlide = 0;
+  public animateDirection;
 
-  public ngOnInit(): void {}
+  public ngOnInit(): void {
+    this.onWindowResize();
+  }
 
   @HostListener('window:resize')
   public onWindowResize() {
     console.log(window.innerWidth);
     if (window.innerWidth >= DEVICE_WIDTH_BREAKPOINTS._8_BOOKS) {
       this.chunkSize = 8;
+    } else if (window.innerWidth >= DEVICE_WIDTH_BREAKPOINTS._7_BOOKS) {
+      this.chunkSize = 7;
+    } else if (window.innerWidth >= DEVICE_WIDTH_BREAKPOINTS._6_BOOKS) {
+      this.chunkSize = 6;
+    } else if (window.innerWidth >= DEVICE_WIDTH_BREAKPOINTS._5_BOOKS) {
+      this.chunkSize = 5;
+    } else if (window.innerWidth >= DEVICE_WIDTH_BREAKPOINTS._4_BOOKS) {
+      this.chunkSize = 4;
+    } else if (window.innerWidth >= DEVICE_WIDTH_BREAKPOINTS._3_BOOKS) {
+      this.chunkSize = 3;
+    } else if (window.innerWidth >= DEVICE_WIDTH_BREAKPOINTS._2_BOOKS) {
+      this.chunkSize = 2;
+    } else {
+      this.chunkSize = 1;
+      this.devicePreviewMode = DEVICE_PREVIEW.MOBILE;
     }
     this.slides = this.chunk(this.books, this.chunkSize);
+    this.lastSlide = this.slides.length - 1;
+    if (this.lastSlide < this.activeSlideIndex) { this.activeSlideIndex = this.lastSlide; }
   }
 
   public chunk(arr, chunkSize) {
@@ -35,5 +73,17 @@ export class CollectionCarouselComponent implements OnInit {
       R.push(arr.slice(i, i + chunkSize));
     }
     return R;
+  }
+
+  public changeSlide(index: number) { this.activeSlideIndex = index; }
+
+  public previousSlide() {
+    if (this.activeSlideIndex > 0) { this.activeSlideIndex--; }
+    this.animateDirection = ANIMATE_DIRECTION.RIGHT;
+  }
+
+  public nextSlide() {
+    if (this.activeSlideIndex < this.lastSlide) { this.activeSlideIndex++; }
+    this.animateDirection = ANIMATE_DIRECTION.LEFT;
   }
 }
