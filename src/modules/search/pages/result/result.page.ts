122c6d73b9d15fe4519903d5273d5115636a3d25
badf00d21 <jiricekova31@gmail.com>
2019-06-17 12:23:18 +0200

    Lil fix

--- src/modules/search/pages/result/result.page.ts
+++ src/modules/search/pages/result/result.page.ts
@@ -2,12 +2,11 @@ import { BooksService } from '../../../core/services/books.service';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Component, HostListener, OnInit } from '@angular/core';
 import { Book } from '../../../core/models/book';
-import { DEVICE_WIDTH_BREAKPOINTS } from '../../../home/components/collection-carousel/collection-carousel.component';
-import { MDBModalRef } from 'ng-uikit-pro-standard';
 
 export enum EDeviceWidth {
   GT_SM = 'gt_sm',
-  LTE_SM = 'lte_sm'
+  LTE_SM = 'lte_sm',
+  LTE_XM = 'lte_xsm'
 }
 
 @Component({
@@ -22,13 +21,11 @@ export class ResultPage implements OnInit {
   private readonly _router: Router;
   public searchResult: Book[];
   public deviceWidth = EDeviceWidth.GT_SM;
-  // public modalRef: MDBModalRef;
 
-  public constructor(booksService: BooksService, activatedRoute: ActivatedRoute, router: Router/*, modalRef: MDBModalRef*/) {
+  public constructor(booksService: BooksService, activatedRoute: ActivatedRoute, router: Router) {
     this._booksService = booksService;
     this._activatedRoute = activatedRoute;
     this._router = router;
-    // this.modalRef = modalRef;
   }
 
   public ngOnInit() {
