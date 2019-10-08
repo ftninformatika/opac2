b0348ded358f80bda8afbfcc65895c178c938b30
badf00d21 <jiricekova31@gmail.com>
2019-10-08 16:35:59 +0200

    [Feature #27] ++

--- src/modules/user/components/admin-collection-editor/admin-collection-editor.ts
+++ src/modules/user/components/admin-collection-editor/admin-collection-editor.ts
@@ -1,8 +1,10 @@
-import { Component, EventEmitter, Input, Output, ViewEncapsulation } from '@angular/core';
+import { Component, EventEmitter, Input, OnDestroy, Output, ViewEncapsulation } from '@angular/core';
 import { BookCollectionModel } from '../../../../models/book-collection.model';
+import { UsersService } from '../../../core/services/users.service';
 import { Book } from '../../../../models/book.model';
 import 'array.prototype.move';
-import {UsersService} from '../../../core/services/users.service';
+import { ToastService } from 'ng-uikit-pro-standard';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'admin-collection-editor',
@@ -10,15 +12,25 @@ import {UsersService} from '../../../core/services/users.service';
   styleUrls: ['admin-collection-editor.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class AdminCollectionEditor {
+export class AdminCollectionEditor implements OnDestroy {
   @Input() collection: BookCollectionModel;
   @Input() books: Book[];
   @Output() deleteCollectionEvent = new EventEmitter<string>();
   @Output() touchedEvent = new EventEmitter<boolean>();
+  public touched: boolean;
   private readonly _userService: UsersService;
+  private readonly _toastService: ToastService;
 
-  public constructor(userService: UsersService) {
+  public constructor(userService: UsersService, toastService: ToastService) {
     this._userService = userService;
+    this._toastService = toastService;
+  }
+
+  public ngOnDestroy(): void {
+    if (this.touched) {
+      console.log('touched');
+      return;
+    }
   }
 
   public deleteBook(collectionId: string) {
@@ -36,6 +48,7 @@ export class AdminCollectionEditor {
     this.books.move(fromIndex, toIndex);
     this.collection.recordsIds.move(fromIndex, toIndex);
     this.touchedEvent.emit(true);
+    this.touched = true;
   }
 
   public removeBook(index: number) {
@@ -52,8 +65,16 @@ export class AdminCollectionEditor {
     if (!this.collection) {
       return;
     }
-    // TODO: Toast messages
-    this._userService.adminCreateModifyCollection(this.collection).subscribe();
+    this._userService.adminCreateModifyCollection(this.collection).subscribe(
+resp => {
+      if (!resp) {
+          this._toastService.warning('Није успело чување промеа у колекцији!');
+        } else {
+          this._toastService.success('Успешно сте променили колецију!');
+        }
+      },
+() => this._toastService.warning('Није успело чување промеа у колекцији!')
+    );
   }
 
 }
