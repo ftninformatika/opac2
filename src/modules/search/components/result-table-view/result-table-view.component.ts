a1739f20582f7e27576d40e9861eb92257d49233
badf00d21 <jiricekova31@gmail.com>
2019-10-11 16:13:10 +0200

    ++

--- src/modules/search/components/result-table-view/result-table-view.component.ts
+++ src/modules/search/components/result-table-view/result-table-view.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input, OnChanges, SimpleChanges, ViewEncapsulation } from '@angular/core';
 import { Book } from '../../../../models/book.model';
 import { IResultPageOptions } from '../../../../models/search/result-page-options.model';
+import { Store } from '@ngxs/store';
+import { ConfigState } from '../../../core/states/config/config.state';
 
 @Component({
   selector: 'result-table-view',
@@ -13,4 +15,11 @@ export class ResultTableView {
   @Input() books: Book[];
   @Input() pageOptions: IResultPageOptions;
   @Input() totalItems: number;
+  private readonly _store: Store;
+  public lib: string;
+
+  public constructor(store: Store) {
+    this._store = store;
+    this.lib = this._store.selectSnapshot(ConfigState.library);
+  }
 }
