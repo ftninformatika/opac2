7535af8f72ad7b61d4094ca131c6dac8b8e8e168
badf00d21 <jiricekova31@gmail.com>
2019-11-20 14:57:27 +0100

    [Task #32] Record preview tabs - bold ISBD, set all inactive tabs default

--- src/modules/book/components/book-tabs/book-tabs.ts
+++ src/modules/book/components/book-tabs/book-tabs.ts
@@ -1,6 +1,7 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild, ViewEncapsulation } from '@angular/core';
 import { Book } from '../../../../models/book.model';
 import { ERecordFormatType } from '../../../core/pipes/record-format.pipe';
+import { TabsetComponent } from 'ng-uikit-pro-standard';
 
 @Component({
   selector: 'book-tabs',
@@ -8,7 +9,28 @@ import { ERecordFormatType } from '../../../core/pipes/record-format.pipe';
   styleUrls: ['book-tabs.scss'],
   encapsulation: ViewEncapsulation.None
 })
-export class BookTabs {
+export class BookTabs implements AfterViewInit {
+  @ViewChild('staticTabs', { static: true }) staticTabs: TabsetComponent;
   @Input() book: Book;
   public RecordFormatType = ERecordFormatType;
+  public activeTabIndex = -1;
+  // TODO: extend to remove content container if needed
+  public showBody = false;
+
+  public ngAfterViewInit(): void {
+    this.staticTabs.tabs[0].active = false;
+  }
+
+  public changeTab(event) {
+    if (!event) {
+      return;
+    }
+    if (event.activeTabIndex !== this.activeTabIndex) {
+      this.activeTabIndex = event.activeTabIndex;
+      this.showBody = true;
+    } else {
+      this.showBody = false;
+    }
+    console.log(event);
+  }
 }
