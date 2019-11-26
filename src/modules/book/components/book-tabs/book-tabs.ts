efdb1351c2acba54c0fb1e43ac3739337f89aa5e
badf00d21 <jiricekova31@gmail.com>
2019-11-26 12:03:13 +0100

    [Task #32] Linked search by authors and other authors

--- src/modules/book/components/book-tabs/book-tabs.ts
+++ src/modules/book/components/book-tabs/book-tabs.ts
@@ -2,6 +2,13 @@ import { AfterViewInit, Component, Input, ViewChild, ViewEncapsulation } from '@
 import { Book } from '../../../../models/book.model';
 import { ERecordFormatType } from '../../../core/pipes/record-format.pipe';
 import { TabsetComponent } from 'ng-uikit-pro-standard';
+import { IResultPageOptionsInitial } from '../../../../models/search/result-page-options.model';
+import { ConfigState } from '../../../core/states/config/config.state';
+import { CryptoUtils } from '../../../../utils/crypto.utils';
+import { Store } from '@ngxs/store';
+import { Router } from '@angular/router';
+import { SearchUtil } from '../../../../utils/search-util';
+import { IPrefixValue } from '../../../../models/prefix-value.model';
 
 @Component({
   selector: 'book-tabs',
@@ -12,10 +19,17 @@ import { TabsetComponent } from 'ng-uikit-pro-standard';
 export class BookTabs implements AfterViewInit {
   @ViewChild('staticTabs', { static: true }) staticTabs: TabsetComponent;
   @Input() book: Book;
+  private readonly _store: Store;
+  private readonly _router: Router;
   public RecordFormatType = ERecordFormatType;
   public activeTabIndex = -1;
   // TODO: extend to remove content container if needed
   public showBody = false;
+
+  public constructor(store: Store, router: Router) {
+    this._store = store;
+    this._router = router;
+  }
 
   public ngAfterViewInit(): void {
     this.staticTabs.tabs[0].active = false;
@@ -31,5 +45,21 @@ export class BookTabs implements AfterViewInit {
     } else {
       this.showBody = false;
     }
+  }
+
+  public async linkedSearch(pref: string, val: string) {
+    if (!pref || !val) {
+      return;
+    }
+    const prefVal: IPrefixValue = {
+      value: val,
+      prefName: pref
+    };
+    const searchModel = SearchUtil.generateSearchModelFromAutoComplete(prefVal);
+    const pageOptions = {...IResultPageOptionsInitial};
+    pageOptions.lib = this._store.selectSnapshot(ConfigState.library);
+    const uriChunk = `query=${JSON.stringify(searchModel)}&pageOptions=${JSON.stringify(pageOptions)}`;
+    const encodedURI = CryptoUtils.encryptData(uriChunk);
+    await this._router.navigate(['/search/result'], {queryParams: {s: encodedURI}});
   }
 }
