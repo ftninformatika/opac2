88caee7317fc969cd1456d893399c8ed9093a0c1
badf00d21 <jiricekova31@gmail.com>
2019-12-02 16:42:42 +0100

    Styling - Page card additional init

--- src/modules/home/components/book-card/book-card.component.ts
+++ src/modules/home/components/book-card/book-card.component.ts
@@ -14,6 +14,7 @@ import { MetaService } from '@ngx-meta/core';
 export class BookCardComponent implements OnInit {
 
   @Input() book: Book;
+  @Input() withButtonsAndInfo: boolean;
   private readonly _router: Router;
   private readonly _store: Store;
   private readonly _metaService: MetaService;
