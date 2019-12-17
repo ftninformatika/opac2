cd15376d424e95da7aa78a5bbcdba852facccd8e
badf00d21 <jiricekova31@gmail.com>
2019-12-17 11:41:47 +0100

    Styling++

--- src/modules/home/components/book-card/book-card.component.ts
+++ src/modules/home/components/book-card/book-card.component.ts
@@ -3,7 +3,7 @@ import { Router } from '@angular/router';
 import { Book } from '../../../../models/book.model';
 import { Store } from '@ngxs/store';
 import { ConfigState } from '../../../core/states/config/config.state';
-import { MetaService } from '@ngx-meta/core';
+import { AddToShelfAction, RemoveFromShelfAction, UserState } from '../../../core/states/user/user.state';
 
 @Component({
   selector: 'book-card',
@@ -17,17 +17,33 @@ export class BookCardComponent implements OnInit {
   @Input() withButtonsAndInfo: boolean;
   private readonly _router: Router;
   private readonly _store: Store;
-  private readonly _metaService: MetaService;
   public lib: string;
+  public isAdmin: boolean;
+  public isOnShelf: boolean;
 
-  public constructor(router: Router, store: Store, metaService: MetaService) {
+  public constructor(router: Router, store: Store) {
     this._router = router;
     this._store = store;
     this.lib = this._store.selectSnapshot(ConfigState.library);
-    this._metaService = metaService;
+    this.isAdmin = this._store.selectSnapshot(UserState.admin);
   }
 
-  public ngOnInit() {}
+  public ngOnInit() {
+    const booksOnShelf = this._store.selectSnapshot(UserState.bookshelfBooksIds);
+    if (this.book && this.book._id && booksOnShelf.indexOf(this.book._id) > -1) {
+      this.isOnShelf = true;
+    }
+  }
+
+  public async removeFromShelf(bookId: string) {
+    await this._store.dispatch(new RemoveFromShelfAction(bookId)).toPromise();
+    this.isOnShelf = false;
+  }
+
+  public async onShelf(bookId: string) {
+    await this._store.dispatch(new AddToShelfAction(bookId)).toPromise();
+    this.isOnShelf = true;
+  }
 
   public async gotoBook(id: string) {
     await this._router.navigate(['/book', this.lib, id]);
