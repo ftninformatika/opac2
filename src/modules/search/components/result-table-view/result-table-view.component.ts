18ee4df95d77fa89846009936022b4e7d505e792
badf00d21 <jiricekova31@gmail.com>
2019-12-17 10:27:12 +0100

    Styling++

--- src/modules/search/components/result-table-view/result-table-view.component.ts
+++ src/modules/search/components/result-table-view/result-table-view.component.ts
@@ -1,8 +1,9 @@
-import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { IResultPageOptions } from '../../../../models/search/result-page-options.model';
 import { ConfigState } from '../../../core/states/config/config.state';
 import { Book } from '../../../../models/book.model';
-import { Store } from '@ngxs/store';
+import { Select, Store } from '@ngxs/store';
+import { AppOptionsState } from '../../../core/states/app-options/app-options.state';
 
 @Component({
   selector: 'result-table-view',
@@ -15,11 +16,17 @@ export class ResultTableView {
   @Input() books: Book[];
   @Input() pageOptions: IResultPageOptions;
   @Input() totalItems: number;
+  @Output() shareChecked = new EventEmitter<string>();
+  @Select(AppOptionsState) optionsState;
   private readonly _store: Store;
   public lib: string;
 
   public constructor(store: Store) {
     this._store = store;
     this.lib = this._store.selectSnapshot(ConfigState.library);
+  }
+
+  public shareCheck(recordId: string) {
+    this.shareChecked.emit(recordId);
   }
 }
